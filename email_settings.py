5d2d5f947b4e84cc7c3ba4dd053b0229cfbd6d43
Kate Alfandari <44461784+alfakat@users.noreply.github.com>
2025-03-07 20:58:12 +0200

    Update email_settings.py

--- email_settings.py
+++ email_settings.py
@@ -1,5 +1,5 @@
-SENDER_EMAIL = "email@gmail.com"
-SENDER_PASSWORD = ""
-RECIPIENT_EMAIL = "email@gmail.com"
+SENDER_EMAIL = "dvrkatja@gmail.com"
+SENDER_PASSWORD = "ljcp ycxd lcce osum"
+RECIPIENT_EMAIL = "dvrkatja@gmail.com"
 SMTP_SERVER = "smtp.gmail.com"
 SMTP_PORT = 465
